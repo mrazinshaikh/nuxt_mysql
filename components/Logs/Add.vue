e13860eb7a2996b0cd1697d35d781140eab23f77
mrazinshaikh <razinshakh8732@gmail.com>
2023-06-26 20:25:02 +0530

    add logs modal and basic form stylings

---
+++ components/Logs/Add.vue
@@ -0,0 +1,76 @@
+<template>
+    <Transition>
+        <div class="fixed inset-0 bg-black/30 ">
+            <div class="rounded absolute left-1/2 top-1/2 sm:top-1/4 -translate-x-1/2 -translate-y-1/2 bg-white w-11/12 sm:w-3/5" v-click-outside="() => $emit('close')">
+                <!-- Header -->
+                <div class="w-full flex justify-between border-b border-gray-700 p-3">
+                    <span>Add new log</span>
+                    <button class="btn !p-0 text-base" @click="$emit('close')">X</button>
+                </div>
+
+                <!-- Main -->
+                <div class="mt-2 p-4">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-5">
+                        <div class="text-left">
+                            <label for="title">Title</label>
+                            <input type="text" id="title" class="form-input" placeholder="Title">
+                        </div>
+                        <div class="text-left">
+                            <label for="amount">Amount</label>
+                            <input type="number" id="amount" class="form-input" placeholder="Amount">
+                        </div>
+                        <div class="text-left">
+                            <label for="description">Description</label>
+                            <textarea class="form-input" id="description" placeholder="Title"></textarea>
+                        </div>
+                        <div class="text-left">
+                            <label for="category">Category</label>
+                            <select class="form-input">
+                                <option value="needs">Needs</option>
+                                <option value="wants">Wants</option>
+                                <option value="savings">Savings</option>
+                                <option value="donation">Donation</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Footer -->
+                <div class="w-full border-t border-gray-700 p-3 flex justify-center gap-4">
+                    <button class="btn btn-primary w-2/5">Save</button>
+                    <button class="btn btn-secondary w-1/5">Reset</button>
+                </div>
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<script>
+export default {
+    name: 'LogsAdd',
+    setup(props){
+
+
+        function test(){
+            console.log('%cTest function called','font-size:30px; color:#ff0000;')
+        }
+
+        return {
+            test
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.15s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
     <header class="border-b border-gray-900 w-full p-4 text-center">
-      <h1 class="text-xl">Daily Logs</h1>
+      <h1 class="text-xl">Expense management</h1>
     </header>
 
-    <main class="px-4">
-      <div></div>
+    <main class="px-4 mt-4">
+      <div class="text-right my-4">
+        <button class="btn btn-primary" @click.stop="showAddModal = true">Add new</button>
+        <LogsAdd v-show="showAddModal" @close="showAddModal = false" />
+      </div>
 
-      <LogsTable :logs="logs" />
+      <LogsList :logs="logs" />
     </main>
 
   </div>
 </template>
 
 <script>
+import LogsList from '~/components/Logs/List.vue';
+import LogsAdd from '~/components/Logs/Add.vue';
+
+
 export default {
   name: "Home",
+  components: {
+    LogsList, LogsAdd,
+  },
   async setup() {
     const logs = ref();
+    const showAddModal = ref(true);
 
     await getAllLogs();
 
@@ -32,6 +43,7 @@ export default {
     return {
       logs,
       getAllLogs,
+      showAddModal,
     };
   },
 };
