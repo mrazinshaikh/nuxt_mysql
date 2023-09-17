b56a957cd1599c243cae412745b18662bfe4c701
mrazinshaikh <razinshakh8732@gmail.com>
2023-09-17 19:15:59 +0530

    primic package version update

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,24 +10,7 @@
 
         <NuxtPage></NuxtPage>
 
-        <footer class="w-full flex flex-col md:flex-row absolute bottom-0 justify-between border-t border-black/50 text-base font-medium mt-10 px-5 md:px-10 py-6 gap-4">
-            <div>Made with <span class="text-red-500">❤️</span> by <NuxtLink class="underline" to="https://github.com/mrazinshaikh" target="_blank" external>Razin Shaikh</NuxtLink>.</div>
-            <NuxtLink
-                to="https://github.com/mrazinshaikh/nuxt_mysql"
-                target="_blank"
-                external
-                class="flex gap-2"
-            >
-                <img
-                    src="/images/github-mark.svg"
-                    height="30"
-                    width="30"
-                    alt="github icon"
-                    class="w-4 object-contain"
-                />
-                <span class="text-sm underline">/mrazinshaikh/nuxt_mysql</span>
-            </NuxtLink>
-        </footer>
+        <Footer />
     </div>
 </template>
 
